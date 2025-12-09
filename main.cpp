9d5737d32155d640b0cbaf6d80c0c47466f097c4
Juan_X <oiundertaker17@gmail.com>
2025-12-09 17:26:17 +0700

    Menambahkan kerangka awal program dan batas layar

--- main.cpp
+++ main.cpp
@@ -1,92 +1,41 @@
 #include <ncurses/curses.h>
+#include <cstdlib>
+#include <vector>
+#include <ctime>
 
-const int lebar = 75;
-const int tinggi = 25;
+using namespace std;
 
-char map[tinggi][lebar+1];
-int px, py; // posisi pemain
+void batas(int lebar, int tinggi) {
+    for(int i = 0; i < lebar; i++) {
+        mvprintw(0, i, "#");
+        mvprintw(tinggi - 1, i, "#");  // Perbaikan: gunakan tinggi-1 untuk border bawah
+    }
 
-// Inisialisasi atau cetak map
-void init_map() {
     for(int i = 0; i < tinggi; i++) {
-        for(int j = 0; j < lebar; j++) {
-            if(i == 0 || i == tinggi - 1 || j == 0 || j == lebar - 1)
-                map[i][j] = '#';
-            else
-                map[i][j] = ' ';
-        }
-        map[i][lebar] = '\0';
+        mvprintw(i, 0, "#");
+        mvprintw(i, lebar - 1, "#");
     }
 }
 
 int main() {
     initscr();
-    cbreak();
     noecho();
-    keypad(stdscr, TRUE);
     curs_set(0);
-    init_map();
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE); 
+    srand(time(NULL));
 
-    px = 2; py = 2;
-    map[py][px] = 'P';
+    int lebar = 40;
+    int tinggi = 20;
+    bool selesai = false;
 
-    // cetak map ke layar
-    for(int cetak = 0; cetak < tinggi; cetak++) {
-        mvprintw(cetak, 0, "%s", map[cetak]);
+    while(!selesai) {
+        clear();
+        batas(lebar, tinggi);
+        refresh();
+        napms(100);
     }
 
-    mvprintw(tinggi, 0, "\n WASD atau panah untuk bergerak dan q untuk keluar");
-    refresh();
-
-    // switch case untuk menggerakkan pemain
-    bool gerak = true;
-    while(gerak) {
-        int gerak2 = getch();
-        int gx = px, gy = py;
-        switch(gerak2) {
-            case 'q':
-            gerak = false;
-            break;
-
-            case KEY_UP:
-            case 'w':
-            gy = py - 1;
-            break;
-
-            case KEY_DOWN:
-            case 's':
-            gy = py + 1;
-            break;
-
-            case KEY_LEFT:
-            case 'a':
-            gx = px - 1;
-            break;
-
-            case KEY_RIGHT:
-            case 'd':
-            gx = px + 1;
-            break;
-
-            default:
-            break;
-        }
-        // Agar bisa berubah posisi
-        if(gx != px || gy != py) {
-            if(gy >= 0 && gy < tinggi && gx >= 0 && gx < lebar) {
-                if(map[gy][gx] != '#') {
-                    map[py][px] = ' ';
-                    px = gx; py = gy;
-                    map[py][px] = 'P';
-                }
-            }
-        }
-
-        // Cetak ulang posisi pemain
-        for(int cetak = 0; cetak < tinggi; cetak++) {
-            mvprintw(cetak, 0, map[cetak]);
-        }
-    }
 
     endwin();
     return 0;
